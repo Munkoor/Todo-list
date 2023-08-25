a55babf7e2bf20bc4fc7d02e53c3cb79860578bd
Munkoor <eee1lvlboss@gmail.com>
2023-08-25 16:40:14 +0300

    Made migrations from models ana registered all models in admin.py

--- list/admin.py
+++ list/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from list.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "deadline", "is_completed"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
